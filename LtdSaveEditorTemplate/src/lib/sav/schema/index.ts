49086e091494388cc21de01b242281209ecb7b92
miiazertyy <miiazerty1238100@example.com>
2026-05-07 21:06:10 +0200

    update readme

--- LtdSaveEditorTemplate/src/lib/sav/schema/index.ts
+++
@@ -1,5 +0,0 @@
-export { MII_SCHEMA } from './mii';
-export { PLAYER_SCHEMA } from './player';
-export { MAP_SCHEMA } from './map';
-export { player, mii } from './branded';
-export { type SchemaLeaf } from './leaf';
--- LtdSaveEditorTemplate/src/routes/sharemii/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import RouteMeta from '$lib/layout/RouteMeta.svelte';
-  import ShareMiiPage from '$lib/shareMii/ShareMiiPage.svelte';
-</script>
-
-<RouteMeta title="Share Mii - LTD Save Editor" />
-
-<ShareMiiPage />
